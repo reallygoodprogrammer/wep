93d4a03a8957c5705dcbf44e472cb648332cbee9
reallygoodprogrammer <milesdustin45@gmail.com>
2025-07-26 13:52:54 -0700

    added traverse options

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # wep
 
-css grep'ing responses using [goquery](https://github.com/PuerkitoBio/goquery)
+scrape responses using [goquery](https://github.com/PuerkitoBio/goquery)
 and [playwright](https://github.com/playwright-community/playwright-go).
-works by going to url, waiting for network to idle, then extracting css-selector
-query content from page.
+Works by going to url, waiting for network to idle, then extracting css-selector
+query content from page. Has the ability to recurse through urls in page.
 
 install with : `go install github.com/reallygoodprogrammer/wep@latest`
 
@@ -22,23 +22,31 @@ wep -u "https://site.com" "div.content > span > a[href]"
 # and p elements with class 'post' from urls in urls.txt file
 wep "h1.title, p.post" < urls.txt
 
+# extract all src values from img attributes and recurse to
+# other pages using a elements href values with class "rec"
+wep -a src -T "a.rec[href]" -A href "img[src]"
+
 # extract all src attribute values from img elements
 wep -a src img < urls.txt
 ```
 
 ```
 Usage of wep:
+  -A string
+    	attribute to match with for '-T' css selector
+  -T string
+    	traverse urls matching css selector arg
   -a string
     	extract from attribute instead of inner content
   -c int
-    	concurrency level (default=3) (default 3)
+    	concurrency level (default 3)
   -headless
     	run in headless mode
   -l string
     	read from local file path instead of making a request
   -s	read html data from standard input
   -t float
-    	timeout for requests (default=10) (default 10)
+    	timeout for requests (default 10)
   -u string
     	site url for request
 ```
--- main.go
+++ main.go
@@ -6,26 +6,33 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/playwright-community/playwright-go"
 )
 
 func main() {
+	// arguments:
 	url := flag.String("u", "", "site url for request")
-	con := flag.Int("c", 3, "concurrency level (default=3)")
-	timeout := flag.Float64("t", 10.0, "timeout for requests (default=10)")
+	con := flag.Int("c", 3, "concurrency level")
+	timeout := flag.Float64("t", 10.0, "timeout for requests")
 	attr := flag.String("a", "", "extract from attribute instead of inner content")
 	headless := flag.Bool("headless", false, "run in headless mode")
 	local := flag.String("l", "", "read from local file path instead of making a request")
 	stdinput := flag.Bool("s", false, "read html data from standard input")
+	traverse_css := flag.String("T", "", "traverse urls matching css selector arg")
+	traverse_attr := flag.String("A", "", "attribute to match with for '-T' css selector")
+
 	flag.Parse()
 
 	query := strings.Join(flag.Args(), " ")
 
+	// create channel for stdout
 	output := make(chan string)
 	var outWg sync.WaitGroup
 	outWg.Add(1)
@@ -37,43 +44,70 @@ func main() {
 		}
 	}()
 
-	process_content := func(content []byte) {
+	toOut := func(outputString string, url string) {
+		output <- outputString
+	}
+
+	errOut := make(chan string)
+	var errWg sync.WaitGroup
+	errWg.Add(1)
+	go func() {
+		defer errWg.Done()
+		for out := range errOut {
+			fmt.Fprintf(os.Stderr, "%s\n", out)
+		}
+	}()
+
+	toErr := func(errString string, url string) {
+		errOut <- errString
+	}
+
+	// create channel for new urls (when traversing)
+	urls := make(chan string)
+	var count int64 = 0
+
+	// process page content
+	process_content := func(content []byte, url string) {
 		doc, err := goquery.NewDocumentFromReader(bytes.NewReader(content))
 		if err != nil {
-			output <- fmt.Sprintf("'%v' could not make reader:\n\t%v", url, err)
+			toErr(fmt.Sprintf("'%v' could not make reader:\n\t%v", err), url) 
 			return
 		}
 
 		doc.Find(query).Each(func(i int, s *goquery.Selection) {
 			html, err := s.Html()
 			if err != nil {
-				output <- fmt.Sprintf("'%v' could not get inner html:\n\t%v", url, err)
+				toErr(fmt.Sprintf("'%v' could not get inner html:\n\t%v", url, err), url)
 			} else if *attr != "" {
 				val, valid := s.Attr(*attr)
 				if valid {
-					output <- val
+					toOut(val, url)
 				}
 			} else {
-				output <- html
+				toOut(html, url)
 			}
 		})
 	}
 
+	// if reading from local file, just process the document
 	if *local != "" {
 		content, err := os.ReadFile(*local)
 		if err != nil {
-			output <- fmt.Sprintf("'%s' could not read file:\n\t%v", *local, err)
+			toErr(fmt.Sprintf("'%s' could not read file:\n\t%v", *local, err), "local-file")
 		} else {
-			process_content(content)
+			process_content(content, "local-file")
 		}
 	} else if *stdinput {
 		content, err := io.ReadAll(os.Stdin)
 		if err != nil {
-			output <- fmt.Sprintf("'stdin' could not read stdin:\n\t%v", err)
+			toErr(fmt.Sprintf("could not read stdin:\n\t%v", err), "stdin")
 		} else {
-			process_content(content)
+			func() {
+				process_content(content, "stdin")
+			}()
 		}
 	} else {
+		// start playwright
 		pw, err := playwright.Run()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "could not start playwright:\n\t%v\n", err)
@@ -91,60 +125,157 @@ func main() {
 			os.Exit(1)
 		}
 
-		urls := make(chan string)
+		// make timeout in seconds
 		*timeout *= 1000
+
+		// diminish the work counter, if zero there are no more urls
+		diminish := func() {
+			if atomic.AddInt64(&count, -1) == 0 {
+				close(urls)
+			}
+		}
+
+		// declaring runit here so it can be called in traverse
+		var runit func(url string)
+		visited := make(map[string]bool)
+		var visitedLock sync.Mutex
+
+		inVisited := func(url string) bool {
+			visitedLock.Lock()
+			defer visitedLock.Unlock()
+
+			if !visited[url] {
+				visited[url] = true
+				return false
+			}
+			return true
+		}
+
+		// find matching traversal urls and add them to urls channel (or process them)
+		traverse := func(content []byte, url string) {
+			doc, err := goquery.NewDocumentFromReader(bytes.NewReader(content))
+			if err != nil {
+				toErr(fmt.Sprintf("could not make reader for traversal:\n\t%v", err), url)
+				return
+			}
+
+			doc.Find(*traverse_css).Each(func(i int, s *goquery.Selection) {
+				html, err := s.Html()
+				if err != nil {
+					toErr(fmt.Sprintf("could not get inner html:\n\t%v", err), url)
+				} else if *traverse_attr != "" {
+					val, valid := s.Attr(*attr)
+					if valid {
+						atomic.AddInt64(&count, 1)
+						traverse_val := absolute_url(url, val)
+						if traverse_val != "" && !inVisited(traverse_val) {
+							select {
+							case urls <- traverse_val:
+								// i love go
+							default:
+								runit(traverse_val)
+							}
+						}
+					}
+				} else {
+					atomic.AddInt64(&count, 1)
+					traverse_val := absolute_url(url, html)
+					if traverse_val != "" && !inVisited(traverse_val) {
+						select {
+						case urls <- traverse_val:
+							// go loves u
+						default:
+							runit(traverse_val)
+						}
+					}
+				}
+			})
+		}
+
+		runit = func(url string) {
+			defer diminish()
+			page, err := browser.NewPage()
+			if err != nil {
+				toErr(fmt.Sprintf("could not create page:\n\t%v", err), url)
+				return
+			}
+
+			_, err = page.Goto(url, playwright.PageGotoOptions{
+				Timeout: playwright.Float(*timeout),
+			})
+			if err != nil {
+				toErr(fmt.Sprintf("could not go to page:\n\t%v", err), url)
+				return
+			}
+
+			ni := playwright.LoadState("networkidle")
+			err = page.WaitForLoadState(playwright.PageWaitForLoadStateOptions{
+				State:   &ni,
+				Timeout: timeout,
+			})
+
+			content, err := page.Content()
+			if err != nil {
+				toErr(fmt.Sprintf("could not get page content:\n\t%v", err), url)
+				return
+			}
+
+			process_content([]byte(content), url)
+			page.Close()
+
+			traverse([]byte(content), url)
+		}
+
+		worker := func() {
+			for url := range urls {
+				func() {
+					runit(url)
+				}()
+			}
+		}
 
 		var inWg sync.WaitGroup
 		for i := 0; i < *con; i++ {
 			inWg.Add(1)
 			go func() {
 				defer inWg.Done()
-				for url := range urls {
-					page, err := browser.NewPage()
-					if err != nil {
-						output <- fmt.Sprintf("'%v' could not create page:\n\t%v", url, err)
-						return
-					}
-
-					_, err = page.Goto(url, playwright.PageGotoOptions{
-						Timeout:   playwright.Float(*timeout),
-					})
-					if err != nil {
-						output <- fmt.Sprintf("'%v' could not go to page:\n\t%v", url, err)
-						return
-					}
-
-					ni := playwright.LoadState("networkidle")
-					err = page.WaitForLoadState(playwright.PageWaitForLoadStateOptions{
-						State: &ni,
-						Timeout: timeout,
-					})
-
-					content, err := page.Content()
-					if err != nil {
-						output <- fmt.Sprintf("'%v' could not get page content:\n\t%v", url, err)
-						return
-					}
-
-					process_content([]byte(content))
-					page.Close()
-				}
+				worker()
 			}()
 		}
 
 		if *url != "" {
+			atomic.AddInt64(&count, 1)
 			urls <- *url
 		} else {
 			scanner := bufio.NewScanner(os.Stdin)
 			for scanner.Scan() {
+				atomic.AddInt64(&count, 1)
 				urls <- scanner.Text()
 			}
 		}
 
-		close(urls)
 		inWg.Wait()
 	}
 
 	close(output)
 	outWg.Wait()
+}
+
+func absolute_url(base string, relative string) string {
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+
+	relativeUrl, err := url.Parse(relative)
+	if err != nil {
+		return ""
+	}
+
+	resolved := baseUrl.ResolveReference(relativeUrl).String()
+
+	if resolved == base {
+		return ""
+	}
+	return resolved
 }
